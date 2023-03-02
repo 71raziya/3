d434a826eaad1cf4d8572e3005e877aa16ba82cd
Элиза <elayza@MacBook-Pro-Eliza.local>
2023-03-02 22:20:52 +0600

    homeW3

---
+++ app.js
@@ -0,0 +1,14 @@
+var vavs = ['milk','beer','beer','milk','milk'];
+var fvfs = 'milk'
+var sas = 'beer'
+var i = 0
+while(i <= 5){
+    if(vavs[i] === fvfs)
+    console.log('good',i)
+    else if(vavs[i] === sas)
+    console.log('bad',i)
+    i++
+}
+
+
+
